0532b9dda35fdac771187cbcf00c519c4223ca85
Tumbl3W3ed <71217381+Tumbl3W3ed@users.noreply.github.com>
2020-11-28 10:53:29 +0200

    completed 2.1

---
+++ POE/MeleeWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    internal class MeleeWeapon : Weapon
+    {
+        public enum Types { Dagger, LongSword}
+
+        public MeleeWeapon(int damage, int range, int durability, int cost, int weaponType, int x = 0, int y = 0) : base(damage, range, durability, cost, weaponType, x, y)
+        {
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
